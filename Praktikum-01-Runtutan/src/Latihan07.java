8f888c67b53980444d20dd91cdd5276a1577a364
BagasAji681 <114287587+BagasAji681@users.noreply.github.com>
2022-09-24 17:17:13 +0700

    Add files via upload

---
+++ Praktikum-01-Runtutan/src/Latihan07.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Latihan07 {
+
+	public static void main(String[] args) {
+		String nama;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Masukan Nama Anda : ");
+		nama = input.nextLine();
+		System.out.println(nama.replaceAll("[aiueo]", "X")); 
+	}
+
+}
